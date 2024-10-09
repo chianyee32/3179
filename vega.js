0691357929d022fbb355565e9fd579b193c6440b
chianyee32 <128032454+chianyee32@users.noreply.github.com>
2024-10-10 03:41:50 +0800

    Add files via upload

---
+++ vega.js
@@ -0,0 +1,25 @@
+var vg_1 = "map.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "high.json";
+vegaEmbed("#high", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "low.json";
+vegaEmbed("#low", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "highest_vs_average.json";
+vegaEmbed("#highest_vs_average", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "difference.json";
+vegaEmbed("#difference", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
