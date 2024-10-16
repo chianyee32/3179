3d2b0e9eda453e9be0121a4f62fb03cab3c60944
chianyee32 <128032454+chianyee32@users.noreply.github.com>
2024-10-16 22:44:48 +0800

    Add files via upload

--- vega.js
+++ vega.js
@@ -23,3 +23,52 @@ vegaEmbed("#difference", vg_5).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
+var vg_6 = "range.json";
+vegaEmbed("#range", vg_6).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+fetch('map2.json')
+  .then(response => response.json())
+  .then(spec => {
+    vegaEmbed('#map2', spec).then(res => {
+        const view = res.view;
+        let isPaused = false; 
+        let currentYear = 1980;
+        let interval;
+
+        const updateYear = () => {
+            if (currentYear > 2029) {
+                currentYear = 1980; 
+            } else {
+                view.signal('year', currentYear).runAsync();
+                currentYear++;
+            }
+        };
+
+        const startAnimation = () => {
+            interval = setInterval(() => {
+                if (!isPaused) {
+                    updateYear();
+                }
+            }, 500); 
+        };
+
+        startAnimation();
+
+        document.getElementById('toggleAnimation').addEventListener('click', () => {
+            isPaused = !isPaused;
+            document.getElementById('toggleAnimation').textContent = isPaused ? 'Resume' : 'Pause';
+            if (!isPaused && !interval) {
+                startAnimation(); 
+            }
+        });
+
+    }).catch(console.error);
+});
+
+
+var vg_8 = "salary_scatter.json";
+vegaEmbed("#salary_scatter", vg_8).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
